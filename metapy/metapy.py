95fa4d476583ad69c45fe24f98248a1c7d8ea269
Dominic Parent <dominic.parent@statcan.gc.ca>
2022-05-13 07:51:06 -0400

    Commented code and removed main related code.

--- metapy/metapy.py
+++ metapy/metapy.py
@@ -2,18 +2,21 @@ import argparse
 import json
 import os
 
+# Takes a path and loads it from json to python dictionaries.
 def read_meta(path_to_file):
     with open(path_to_file, "r") as f:
         meta = json.load(f)
 
     return meta
 
+# Writes a python dictionary to an indented json file.
 def write_meta(meta, path_to_file):
     json_meta = json.dumps(meta, indent = 4)
 
     with open(path_to_file, "w") as f:
         f.write(json_meta)
 
+# From a root folder, explorer all subfolders for all metadata.json files.
 def find_metafiles(root_path):
     metafiles = []
     for (root,dirs,files) in os.walk(root_path):
@@ -22,25 +25,13 @@ def find_metafiles(root_path):
 
     return metafiles
 
+# Not currently implemented.
+# Look through a metafile for a search_key and search_value.
+def search_metafiles(metafile, search_key, search_value):
+    pass
+
+# Returns an empty set of dictionaries that can be serialized to a standard-compliant metadata.json file.
 def init_meta():
     metafile_init = {"dataset":{"title":"", "description":"", "keyword":[], "modified":"", "publisher":{"name":""}, "contactPoint":{"fn":""}, "identifier":"", "refPeriod":""}}
 
     return metafile_init
-
-def main(args):
-    print("It's alive!")
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Metadata interaction tool.')
-    parser.add_argument('-o',
-                        '--output',
-                        dest='output',
-                        action='store',
-                        help='Path to output file.')
-    parser.add_argument('-s',
-                        '--substitution',
-                        dest='subs',
-                        action='store',
-                        help='Path to JSON file of substitutions.')
-    args = parser.parse_args()
-    main(args)
