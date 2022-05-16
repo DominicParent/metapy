6980f7392ec931e167169bf1ffd43ea53139bb13
Dominic Parent <dominic.parent@statcan.gc.ca>
2022-05-16 06:58:06 -0400

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'Python-based metadata management tool.'
+
+setup(
+    name="metapy",
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[]
+)
